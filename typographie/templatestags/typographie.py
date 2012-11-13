dee1fc2de9a7f6ca23a16e778a1cb93a8fe1562f
Jean-Hugues Pinson <jh.pinson@oxys.net>
2012-11-13 15:30:45 +0100

    spaces,ellipsis, widont

--- typographie/templatestags/typographie.py
+++ typographie/templatestags/typographie.py
@@ -5,25 +5,74 @@ from django.utils.encoding import smart_str, force_unicode
 import re
 register = template.Library()
 
+spaces_rules = {
+    u':' : u'\xa0: ',
+    u',' : u', ',
+    u'.' : u'. ',
+    u'\u2026' : u'\u2026 ',
+    u';' : u'\xa0; ',
+    u'!' : u'\xa0! ',
+    u'?' : u'\xa0? ',
+    u'(' : u' (',
+    u')' : u') ',
+    u'[' : u' [',
+    u']' : u'] ',
+    u'\xab' : u' \xab\xa0',
+    u'\xbb' : u'\xa0\xbb '
+    
+  }
 
-@register.filter
-@stringfilter
-def ellipsis(text):
-  pass
+spaces_rules_chars = u''.join([ u"%s%s" % ( (u'\\' if c in [u'-', u'.', u'(', u')', u'[', u']'] else u''), c ) for c in spaces_rules.keys()])
+
+re_clean_space_1 = re.compile(u'\s*(<[^>]+>|)([%s])(<[^>]+>|)\s*' % spaces_rules_chars, flags=re.U)
+re_clean_space_2 = re.compile(u'([%s])\s*(<[^>]+>|)(<[^>]+>|)\s*' % spaces_rules_chars, flags=re.U)
+re_clean_space_3 = re.compile(u'\s*(<[^>]+>|)(<[^>]+>|)\s*([%s])' % spaces_rules_chars, flags=re.U)
+
+re_replace_chars_with_spaces = re.compile(u'[%s]' % spaces_rules_chars, flags=re.U)
+
+widont_finder = re.compile(r"""((?:</?(?:a|em|span|strong|i|b)[^>]*>)|[^<>\s]) # must be proceeded by an approved inline opening or closing tag or a nontag/nonspace
+                                   \s+                                             # the space to replace
+                                   ([^<>\s]+                                       # must be flollowed by non-tag non-space characters
+                                   \s*                                             # optional white space! 
+                                   (</(a|em|span|strong|i|b)>\s*)*                 # optional closing inline tags with optional white space after each
+                                   ((</(p|h[1-6]|li|dt|dd)>)|$))                   # end with a closing p, h1-6, li or the end of the string
+                                   """, re.VERBOSE)
+
+def replace_with_spaces(matchobj):
+    return spaces_rules.get(matchobj.group(0))
 
 @register.filter
 @stringfilter
 def spaces(text):
-  text = re.sub(r'\s*(<[^>]+>|):(<[^>]+>|)\s*',u':\\1\\2',text,flags=re.U)
-  text = re.sub(r':\s*(<[^>]+>|)(<[^>]+>|)\s*',u':\\1\\2',text,flags=re.U)
-  text = re.sub(r'\s*(<[^>]+>|)(<[^>]+>|)\s*:',u':\\1\\2',text,flags=re.U)
-  text = re.sub(r':',u'\xa0: ',text,flags=re.U)
+  # clean spaces  
+  text = re_clean_space_1.sub(u'\\2\\1\\3',text)
+  text = re_clean_space_2.sub(u'\\1\\2\\3',text)
+  text = re_clean_space_3.sub(u'\\3\\1\\2',text)
+  
+  # replace chars with chars + spaces
+  text = re_replace_chars_with_spaces.sub(replace_with_spaces ,text)
+  
   return text
+
+def widont(text):
+    
+    text = widont_finder.sub(u'\\1\xa0\\2', text)
+    return text
+
+
+@register.filter
+@stringfilter
+def ellipsis(text):
+  text = re.sub(r"\.\.\.", u"\u2026", text)
+  text = re.sub(r"\. \. \.", u"\u2026", text)
+  return text
+
 
 @register.filter
 @stringfilter
 def typographie(text):
   text = force_unicode(text)
+  text = ellipsis(text)
   text = spaces(text)
 
   return text
--- typographie/tests.py
+++ typographie/tests.py
@@ -3,48 +3,298 @@ from templatestags.typographie import *
 
 class TypographieTestCase(unittest.TestCase):
 
-  def test_spaces_text(self):
-    tests = [
-              u"Deux points:ok",
-              u"Deux points\xa0:ok",
-              u"Deux points\xa0:\xa0ok",
-              u"Deux points :ok",
-              u"Deux points : ok", 
-              u"Deux points\xa0: ok",
-              u"Deux points :\xa0ok",
-              u"Deux points\xa0:\nok",
-              u"Deux points\xa0:  ok",
-            ]
-    result = u"Deux points\xa0: ok"
+
+  def get_text_tests(self, c):
+      
+      return [test % c for test in  [
+              u"Du texte%sok",
+              u"Du texte\xa0%sok",
+              u"Du texte\xa0%s\xa0ok",
+              u"Du texte %sok",
+              u"Du texte %s ok", 
+              u"Du texte\xa0%s ok",
+              u"Du texte %s\xa0ok",
+              u"Du texte\xa0%s\nok",
+              u"Du texte\xa0%s  ok",
+            ]]
+  
+  def get_html_tests(self, c):
+      
+      return [test % c for test in  [
+              u"<b>Du texte</b>%s<b>ok</b>",
+              u"<b>Du texte </b>%s<b>ok</b>",
+              u"<b>Du texte </b>%s<b> ok</b>",
+              u"<b>Du texte</b>%s<b> ok</b>",
+
+              u"<b>Du texte%s</b><b>ok</b>",
+              u"<b>Du texte %s</b><b>ok</b>",
+              u"<b>Du texte %s</b><b> ok</b>",
+              u"<b>Du texte%s</b><b> ok</b>",
+
+              u"<b>Du texte</b><b>%sok</b>",
+              u"<b>Du texte </b><b>%sok</b>",
+              u"<b>Du texte </b><b> %sok</b>",
+              u"<b>Du texte</b><b>%s ok</b>",
+            ]]
+  
+  def get_html_tests_2(self, c):
+      
+      return [test % c for test in [
+              u"<b>Du texte</b>%s<b>ok </b>",
+            ]]
+  
+  def test_typographie_deux_points_text(self):
+    
+    tests = self.get_text_tests(u':')
+    result = u"Du texte\xa0: ok"
     for test in tests:
-      self.assertEqual(spaces(test),result)
+      self.assertEqual(typographie(test),result)
 
-  def test_spaces_html(self):
-    tests = [
-              u"<b>Deux points</b>:<b>ok</b>",
-              u"<b>Deux points </b>:<b>ok</b>",
-              u"<b>Deux points </b>:<b> ok</b>",
-              u"<b>Deux points</b>:<b> ok</b>",
 
-							u"<b>Deux points:</b><b>ok</b>",
-              u"<b>Deux points :</b><b>ok</b>",
-              u"<b>Deux points :</b><b> ok</b>",
-              u"<b>Deux points:</b><b> ok</b>",
-
-							u"<b>Deux points</b><b>:ok</b>",
-              u"<b>Deux points </b><b>:ok</b>",
-              u"<b>Deux points </b><b> :ok</b>",
-              u"<b>Deux points</b><b>: ok</b>",
-            ]
-    result = u"<b>Deux points\xa0: </b><b>ok</b>"
+  def test_typographie_virgule_text(self):
+    tests = self.get_text_tests(u',')
+    result = u"Du texte, ok"
     for test in tests:
-      self.assertEqual(spaces(test),result)
-
-
-    tests = [
-              u"<b>Deux points</b>:<b>ok </b>",
-            ]
-    result = u"<b>Deux points\xa0: </b><b>ok </b>"
+      self.assertEqual(typographie(test),result)
+      
+  def test_typographie_point_text(self):
+    tests = self.get_text_tests(u'.')
+    result = u"Du texte. ok"
     for test in tests:
-      self.assertEqual(spaces(test),result)
+      self.assertEqual(typographie(test),result)
+      
+  def test_typographie_point_virgule_text(self):
+    
+    tests = self.get_text_tests(u';')
+    result = u"Du texte\xa0; ok"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+      
+  
+  def test_typographie_point_exclamation_text(self):
+    
+    tests = self.get_text_tests(u'!')
+    result = u"Du texte\xa0! ok"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+      
+      
+  def test_typographie_point_interrogation_text(self):
+    
+    tests = self.get_text_tests(u'?')
+    result = u"Du texte\xa0? ok"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+      
+  def test_typographie_points_suspensions_text(self):
+    
+    tests = self.get_text_tests(u'\u2026')
+    result = u"Du texte\u2026 ok"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+   
+  def test_typographie_points_suspensions_text_2(self):
+    
+    tests = self.get_text_tests(u'...')
+    result = u"Du texte\u2026 ok"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+   
+  def test_typographie_paranthese_ouvrante_text(self):
+    
+    tests = self.get_text_tests(u'(')
+    result = u"Du texte (ok"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+      
+  def test_typographie_crochet_ouvrant_text(self):
+    
+    tests = self.get_text_tests(u'[')
+    result = u"Du texte [ok"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+      
+  def test_typographie_paranthese_fermante_text(self):
+    
+    tests = self.get_text_tests(u')')
+    result = u"Du texte) ok"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+      
+  def test_typographie_crochet_fermant_text(self):
+    
+    tests = self.get_text_tests(u']')
+    result = u"Du texte] ok"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+      
+  def test_typographie_guillemet_ouvrant_text(self):
+    
+    tests = self.get_text_tests(u'\xab')
+    result = u"Du texte \xab\xa0ok"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+      
+  def test_typographie_guillemet_fermant_text(self):
+    
+    tests = self.get_text_tests(u'\xbb')
+    result = u"Du texte\xa0\xbb ok"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+      
+  def  test_typographie_deux_points_html(self):
+    tests = self.get_html_tests(u':')
+    result = u"<b>Du texte\xa0: </b><b>ok</b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
 
+    tests =self.get_html_tests_2(u':')
+    result = u"<b>Du texte\xa0: </b><b>ok </b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+      
+  def  test_typographie_virgule_html(self):
+    tests = self.get_html_tests(u',')
+    result = u"<b>Du texte, </b><b>ok</b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+
+    tests =self.get_html_tests_2(u',')
+    result = u"<b>Du texte, </b><b>ok </b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+      
+  def  test_typographie_point_html(self):
+    tests = self.get_html_tests(u'.')
+    result = u"<b>Du texte. </b><b>ok</b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+
+    tests =self.get_html_tests_2(u'.')
+    result = u"<b>Du texte. </b><b>ok </b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+      
+      
+  def  test_typographie_point_virgule_html(self):
+    tests = self.get_html_tests(u';')
+    result = u"<b>Du texte\xa0; </b><b>ok</b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+
+    tests =self.get_html_tests_2(u';')
+    result = u"<b>Du texte\xa0; </b><b>ok </b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+
+  def  test_typographie_point_exclamation_html(self):
+    tests = self.get_html_tests(u'!')
+    result = u"<b>Du texte\xa0! </b><b>ok</b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+
+    tests =self.get_html_tests_2(u'!')
+    result = u"<b>Du texte\xa0! </b><b>ok </b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+
+
+  def  test_typographie_point_interrogation_html(self):
+    tests = self.get_html_tests(u'?')
+    result = u"<b>Du texte\xa0? </b><b>ok</b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+
+    tests =self.get_html_tests_2(u'?')
+    result = u"<b>Du texte\xa0? </b><b>ok </b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+      
+      
+  def  test_typographie_points_suspensions_html(self):
+    tests = self.get_html_tests(u'\u2026')
+    result = u"<b>Du texte\u2026 </b><b>ok</b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+
+    tests =self.get_html_tests_2(u'\u2026')
+    result = u"<b>Du texte\u2026 </b><b>ok </b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+      
+  def  test_typographie_points_suspensions_html_2(self):
+    tests = self.get_html_tests(u'...')
+    result = u"<b>Du texte\u2026 </b><b>ok</b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+
+    tests =self.get_html_tests_2(u'...')
+    result = u"<b>Du texte\u2026 </b><b>ok </b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+      
+  def  test_typographie_paranthese_ouvrante_html(self):
+    tests = self.get_html_tests(u'(')
+    result = u"<b>Du texte (</b><b>ok</b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+
+    tests =self.get_html_tests_2(u'(')
+    result = u"<b>Du texte (</b><b>ok </b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+      
+  def  test_typographie_crochet_ouvrant_html(self):
+    tests = self.get_html_tests(u'[')
+    result = u"<b>Du texte [</b><b>ok</b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+
+    tests =self.get_html_tests_2(u'[')
+    result = u"<b>Du texte [</b><b>ok </b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+      
+  def  test_typographie_paranthese_fermante_html(self):
+    tests = self.get_html_tests(u')')
+    result = u"<b>Du texte) </b><b>ok</b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+
+    tests =self.get_html_tests_2(u')')
+    result = u"<b>Du texte) </b><b>ok </b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+      
+  def  test_typographie_crochet_fermant_html(self):
+    tests = self.get_html_tests(u']')
+    result = u"<b>Du texte] </b><b>ok</b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+
+    tests =self.get_html_tests_2(u']')
+    result = u"<b>Du texte] </b><b>ok </b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+      
+  def  test_typographie_guillemet_ouvrant_html(self):
+    tests = self.get_html_tests(u'\xab')
+    result = u"<b>Du texte \xab\xa0</b><b>ok</b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+
+    tests =self.get_html_tests_2(u'\xab')
+    result = u"<b>Du texte \xab\xa0</b><b>ok </b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+      
+  def  test_typographie_guillemet_fermant_html(self):
+    tests = self.get_html_tests(u'\xbb')
+    result = u"<b>Du texte\xa0\xbb </b><b>ok</b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
+
+    tests =self.get_html_tests_2(u'\xbb')
+    result = u"<b>Du texte\xa0\xbb </b><b>ok </b>"
+    for test in tests:
+      self.assertEqual(typographie(test),result)
