469e98c8a48b2ac8e8f995b06663229804770316
Jean-Hugues Pinson <jh.pinson@oxys.net>
2012-11-14 15:56:04 +0100

    bugfix + smartypants

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 django-typographie
 ==================
 
-Un équivalent de typogrify pour le français
+Un équivalent de typogrify pour le français.
+
+django-typographie contient une version modifiée de smartypants (Jeff Schenck, https://github.com/jeffschenck/python-smartypants), ainsi que le méthode widont provenant de django-typogrify  (Chris Drackett, https://github.com/chrisdrackett/django-typogrify)
+
--- typographie/templatetags/typographie.py
+++ typographie/templatetags/typographie.py
@@ -1,6 +1,7 @@
 from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.encoding import smart_str, force_unicode
+from smartypants import smartyPants
 
 import re
 from django.utils.safestring import mark_safe
@@ -24,13 +25,14 @@ spaces_rules = {
     
   }
 
+
 spaces_rules_chars = u''.join([ u"%s%s" % ( (u'\\' if c in [u'-', u'.', u'(', u')', u'[', u']'] else u''), c ) for c in spaces_rules.keys()])
 
 re_clean_space_1 = re.compile(u'\s*(<[^>]+>|)([%s])(<[^>]+>|)\s*' % spaces_rules_chars, flags=re.U)
 re_clean_space_2 = re.compile(u'([%s])\s*(<[^>]+>|)(<[^>]+>|)\s*' % spaces_rules_chars, flags=re.U)
 re_clean_space_3 = re.compile(u'\s*(<[^>]+>|)(<[^>]+>|)\s*([%s])' % spaces_rules_chars, flags=re.U)
 
-re_replace_chars_with_spaces = re.compile(u'[%s]' % spaces_rules_chars, flags=re.U)
+re_replace_chars_with_spaces = re.compile(u'\s*[%s]\s*' % spaces_rules_chars, flags=re.U)
 
 widont_finder = re.compile(r"""((?:</?(?:a|em|span|strong|i|b)[^>]*>)|[^<>\s]) # must be proceeded by an approved inline opening or closing tag or a nontag/nonspace
                                    \s+                                             # the space to replace
@@ -43,16 +45,42 @@ widont_finder = re.compile(r"""((?:</?(?:a|em|span|strong|i|b)[^>]*>)|[^<>\s]) #
 def replace_with_spaces(matchobj):
     return spaces_rules.get(matchobj.group(0))
 
+def replace_content(matchobj):
+    text = spaces(matchobj.group(4).strip())
+    
+    ## clean spaces  
+    #text = re_clean_space_1.sub(u'\\2\\1\\3',text)
+    #text = re_clean_space_2.sub(u'\\1\\2\\3',text)
+    #text = re_clean_space_3.sub(u'\\3\\1\\2',text)
+    
+    return u"%s%s%s%s%s" % (matchobj.group(1), matchobj.group(2),matchobj.group(3), text,matchobj.group(5))
+    
+re_parse_content = re.compile(r'(<[^>]* ?)((?:div|p|pre|blockquote))( ?[^>]*>)(.*?)(</\2>)', flags = re.S + re.U)
 
 def spaces(text):
-  # clean spaces  
-  text = re_clean_space_1.sub(u'\\2\\1\\3',text)
-  text = re_clean_space_2.sub(u'\\1\\2\\3',text)
-  text = re_clean_space_3.sub(u'\\3\\1\\2',text)
-  
-  # replace chars with chars + spaces
-  text = re_replace_chars_with_spaces.sub(replace_with_spaces ,text)
-  
+    
+  if re_parse_content.match(text) is not None:
+      text = re_parse_content.sub(replace_content, text)
+  else:
+    
+      # clean spaces  
+      text = re_clean_space_1.sub(u'\\2\\1\\3',text)
+      text = re_clean_space_2.sub(u'\\1\\2\\3',text)
+      text = re_clean_space_3.sub(u'\\3\\1\\2',text)
+      
+      # replace chars with chars + spaces
+      text = re_replace_chars_with_spaces.sub(replace_with_spaces ,text)
+      
+      def test(m):
+          r = " "
+          if m.group(3) == u'\xa0':
+              r = u'\xa0'
+              
+          return u"%s%s%s" % (m.group(1), r, m.group(5))
+      
+      text = re.compile(r'([%s])(\s*)(\xa0*)(\s*)([%s])' % (spaces_rules_chars, spaces_rules_chars), flags=re.U).sub(test,text)
+      text = re.compile(r'(\S)\s*([\.,])' ).sub(u'\\1\\2',text)
+      
   return text
 
 
@@ -70,7 +98,9 @@ def ellipsis(text):
 @register.filter
 @stringfilter
 def typographie(text):
+  
   text = force_unicode(text)
+  text = smartyPants(text)
   text = ellipsis(text)
   text = spaces(text)
   text = widont(text)
