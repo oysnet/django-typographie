6be8b0ed63d4b33f61250dff78caca337f9f6a69
Jean-Hugues Pinson <jh.pinson@oxys.net>
2012-11-15 11:29:51 +0100

    fix issue #2 - problem with links

--- typographie/templatetags/typographie.py
+++ typographie/templatetags/typographie.py
@@ -32,7 +32,59 @@ re_clean_space_1 = re.compile(u'\s*(<[^>]+>|)([%s])(<[^>]+>|)\s*' % spaces_rules
 re_clean_space_2 = re.compile(u'([%s])\s*(<[^>]+>|)(<[^>]+>|)\s*' % spaces_rules_chars, flags=re.U)
 re_clean_space_3 = re.compile(u'\s*(<[^>]+>|)(<[^>]+>|)\s*([%s])' % spaces_rules_chars, flags=re.U)
 
-re_replace_chars_with_spaces = re.compile(u'\s*[%s]\s*' % spaces_rules_chars, flags=re.U)
+
+# return char + spaces accoring to cb_re_replace_chars_with_spaces
+re_replace_chars_with_spaces = re.compile(u'\s*([%s])\s*' % spaces_rules_chars, flags=re.U)
+def cb_re_replace_chars_with_spaces(matchobj):
+    return spaces_rules.get(matchobj.group(1))
+
+
+# extract content between two tags
+re_content_between_tags = re.compile(r'(>|^)([^<>]*)(<|$)')
+def cb_re_content_between_tags(matchobj):
+          text = matchobj.group(2)
+          text = re_replace_chars_with_spaces.sub(cb_re_replace_chars_with_spaces ,text)
+          return u"%s%s%s" % (matchobj.group(1), text, matchobj.group(3))
+  
+# extract html between tags div, p, pre, blockquote 
+re_parse_content = re.compile(r'(<[^>]* ?)((?:div|p|pre|blockquote))( ?[^>]*>)(.*?)(</\2>)', flags = re.S + re.U)
+def cb_re_parse_content(matchobj):
+    text = spaces(matchobj.group(4).strip())
+    return u"%s%s%s%s%s" % (matchobj.group(1), matchobj.group(2),matchobj.group(3), text,matchobj.group(5))
+
+# used to remove spaces before . and ,
+re_remove_spaces_before_comma_and_dot = re.compile(r'(\S)\s*([\.,])')
+
+# remove multiple spaces between tow typo chars
+re_remove_multiple_spaces = re.compile(r'([%s])(\s*)(\xa0*)(\s*)([%s])' % (spaces_rules_chars, spaces_rules_chars), flags=re.U)
+def cb_re_remove_multiple_spaces(m):
+          r = " "
+          if m.group(3) == u'\xa0':
+              r = u'\xa0'
+              
+          return u"%s%s%s" % (m.group(1), r, m.group(5))
+def spaces(text):
+    
+  if re_parse_content.match(text) is not None:
+      text = re_parse_content.sub(cb_re_parse_content, text)
+  else:
+    
+      # clean spaces  
+      text = re_clean_space_1.sub(u'\\2\\1\\3',text)
+      text = re_clean_space_2.sub(u'\\1\\2\\3',text)
+      text = re_clean_space_3.sub(u'\\3\\1\\2',text)
+      
+      # set spaces
+      text = re_content_between_tags.sub(cb_re_content_between_tags, text)
+      
+      # remove multiple spaces      
+      text = re_remove_multiple_spaces.sub(cb_re_remove_multiple_spaces,text)
+      text = re_remove_spaces_before_comma_and_dot.sub(u'\\1\\2',text)
+      
+      
+      
+  return text
+
 
 widont_finder = re.compile(r"""((?:</?(?:a|em|span|strong|i|b)[^>]*>)|[^<>\s]) # must be proceeded by an approved inline opening or closing tag or a nontag/nonspace
                                    \s+                                             # the space to replace
@@ -41,47 +93,6 @@ widont_finder = re.compile(r"""((?:</?(?:a|em|span|strong|i|b)[^>]*>)|[^<>\s]) #
                                    (</(a|em|span|strong|i|b)>\s*)*                 # optional closing inline tags with optional white space after each
                                    ((</(p|h[1-6]|li|dt|dd)>)|$))                   # end with a closing p, h1-6, li or the end of the string
                                    """, re.VERBOSE)
-
-def replace_with_spaces(matchobj):
-    return spaces_rules.get(matchobj.group(0))
-
-def replace_content(matchobj):
-    text = spaces(matchobj.group(4).strip())
-    
-    ## clean spaces  
-    #text = re_clean_space_1.sub(u'\\2\\1\\3',text)
-    #text = re_clean_space_2.sub(u'\\1\\2\\3',text)
-    #text = re_clean_space_3.sub(u'\\3\\1\\2',text)
-    
-    return u"%s%s%s%s%s" % (matchobj.group(1), matchobj.group(2),matchobj.group(3), text,matchobj.group(5))
-    
-re_parse_content = re.compile(r'(<[^>]* ?)((?:div|p|pre|blockquote))( ?[^>]*>)(.*?)(</\2>)', flags = re.S + re.U)
-
-def spaces(text):
-    
-  if re_parse_content.match(text) is not None:
-      text = re_parse_content.sub(replace_content, text)
-  else:
-    
-      # clean spaces  
-      text = re_clean_space_1.sub(u'\\2\\1\\3',text)
-      text = re_clean_space_2.sub(u'\\1\\2\\3',text)
-      text = re_clean_space_3.sub(u'\\3\\1\\2',text)
-      
-      # replace chars with chars + spaces
-      text = re_replace_chars_with_spaces.sub(replace_with_spaces ,text)
-      
-      def test(m):
-          r = " "
-          if m.group(3) == u'\xa0':
-              r = u'\xa0'
-              
-          return u"%s%s%s" % (m.group(1), r, m.group(5))
-      
-      text = re.compile(r'([%s])(\s*)(\xa0*)(\s*)([%s])' % (spaces_rules_chars, spaces_rules_chars), flags=re.U).sub(test,text)
-      text = re.compile(r'(\S)\s*([\.,])' ).sub(u'\\1\\2',text)
-      
-  return text
 
 
 def widont(text):
